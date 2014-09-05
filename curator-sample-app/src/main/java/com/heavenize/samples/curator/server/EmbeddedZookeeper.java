aa9a68f5e0b47eb4e99aeda8259e6842193e5d36
dzlab <dzlabs@outlook.com>
2014-09-05 13:30:34 +0200

    added apache curator sample app

---
+++ curator-sample-app/src/main/java/com/heavenize/samples/curator/server/EmbeddedZookeeper.java
@@ -0,0 +1,89 @@
+/**
+ * (c) Copyright 2005-2014 Heavenize SAS
+ * 34, rue serpente, 75006 Paris, FRANCE
+ * HEAVENIZE project
+ *
+ * This code is the property of Heavenize SAS
+ * Registration : RCS PARIS B 508 496 528
+ * For any question or license, please contact Heavenize at info@heavenize.com
+ */
+package com.heavenize.samples.curator.server;
+
+import java.lang.reflect.Field;
+import java.nio.channels.ServerSocketChannel;
+
+import javax.management.JMException;
+
+import org.apache.zookeeper.jmx.ManagedUtil;
+import org.apache.zookeeper.server.ServerCnxnFactory;
+import org.apache.zookeeper.server.ServerConfig;
+import org.apache.zookeeper.server.ZooKeeperServerMain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * 
+ * @author dzlab (dzlabs@outlook.com) , 19 août 2014
+ */
+public class EmbeddedZookeeper
+{
+  static Logger               LOG = LoggerFactory.getLogger(EmbeddedZookeeper.class);
+
+  private String              port;
+  private String              directory;
+  private ZooKeeperServerMain zk;
+
+  public EmbeddedZookeeper(String port, String directory)
+  {
+    LOG.info("Storing zookeeper data to {}", directory);
+    this.port = (port != null ? port : "2181");
+    this.directory = directory;
+    this.zk = new ZooKeeperServerMain();
+  }
+
+  public void start() throws Exception
+  {
+    LOG.info("Running zookeeper on thread '{}' with id {}", Thread.currentThread().getName(), Thread.currentThread().getId());
+    try
+    {
+      ManagedUtil.registerLog4jMBeans();
+    }
+    catch (JMException e)
+    {
+      LOG.warn("Unable to register log4j JMX control", e);
+    }
+    ServerConfig config = new ServerConfig();
+    config.parse(new String[] { port, directory });
+    zk.runFromConfig(config);
+  }
+
+  /**
+   * @return the port
+   */
+  public String getPort()
+  {
+    return port;
+  }
+
+  /**
+   * @return the directory
+   */
+  public String getDirectory()
+  {
+    return directory;
+  }
+
+  public void stop() throws Exception
+  {
+    Field cnxnFactoryField = ZooKeeperServerMain.class.getDeclaredField("cnxnFactory");
+    cnxnFactoryField.setAccessible(true);
+    ServerCnxnFactory cnxnFactory = (ServerCnxnFactory) cnxnFactoryField.get(this);
+    cnxnFactory.closeAll();
+
+    Field ssField = cnxnFactory.getClass().getDeclaredField("ss");
+    ssField.setAccessible(true);
+    ServerSocketChannel ss = (ServerSocketChannel) ssField.get(cnxnFactory);
+    ss.close();
+  }
+}
---
+++ curator-sample-app/src/main/java/com/heavenize/samples/curator/server/ZookeeperQuorumManager.java
@@ -0,0 +1,88 @@
+/**
+ * (c) Copyright 2005-2014 Heavenize SAS
+ * 34, rue serpente, 75006 Paris, FRANCE
+ * HEAVENIZE project
+ *
+ * This code is the property of Heavenize SAS
+ * Registration : RCS PARIS B 508 496 528
+ * For any question or license, please contact Heavenize at info@heavenize.com
+ */
+package com.heavenize.samples.curator.server;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.heavenize.samples.curator.commons.ZookeeperUtils;
+
+/**
+ * 
+ * @author dzlab (dzlabs@outlook.com) , 21 août 2014
+ */
+public class ZookeeperQuorumManager implements InitializingBean, DisposableBean
+{
+  /**
+   * Logger for class {@link ZookeeperQuorumManager}.
+   */
+  private final static Logger log = LoggerFactory.getLogger(ZookeeperQuorumManager.class);
+  
+  private final EmbeddedQuorum         quorum;
+
+  private ThreadPoolTaskExecutor taskExecutor;
+
+  public ZookeeperQuorumManager()
+  {
+    this.quorum = new EmbeddedQuorum();
+  }
+  
+  /**
+   * @param taskExecutor the taskExecutor to set
+   */
+  public void setTaskExecutor(ThreadPoolTaskExecutor taskExecutor)
+  {
+    this.taskExecutor = taskExecutor;
+  }
+
+  /* (non-Javadoc)
+   * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+   */
+  @Override
+  public void afterPropertiesSet() throws Exception
+  {
+    //ZKUtils.configure();
+    startQuorum();
+  }
+
+  /* (non-Javadoc)
+   * @see org.springframework.beans.factory.DisposableBean#destroy()
+   */
+  @Override
+  public void destroy() throws Exception
+  {
+    quorum.stop();
+  }
+
+  private void startQuorum()
+  {
+    taskExecutor.execute(new Runnable() {
+      @Override
+      public void run()
+      {
+        log.info("starting zookeeper");
+        try
+        {
+          Properties zkProp = ZookeeperUtils.loadServerProperties();
+          quorum.start(zkProp);
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+}
